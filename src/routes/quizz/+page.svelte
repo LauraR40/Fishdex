6d1cec6ce4adab7355fc425371ce2843759f62d8
Laura <106605150+LauraR40@users.noreply.github.com>
2024-04-23 15:28:23 +0200

    proto quizz (texte trou)

---
+++ src/routes/quizz/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+  import { onMount } from "svelte";
+
+  let answers = ["dolor", "scelerisque", "Nullam"];
+
+  function verifier() {
+    const spans = [...document.querySelectorAll("input.answer")];
+    for (let i = 0; i < spans.length; i++) {
+      const span = spans[i];
+      span.classList.remove("wrong", "right");
+      if (span.value != "...") {
+        if (span.value == answers[i]) {
+          span.classList.add("right");
+        } else {
+          span.classList.add("wrong");
+        }
+      }
+    }
+  }
+</script>
+
+<div class="col" style="padding: 5vw;">
+  <p>
+    Lorem ipsum <input placeholder="..." class="answer" />
+    sit amet, consectetur adipiscing elit. Proin at blandit nisl. Orci varius natoque
+    penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vestibulum,
+    sem nec mattis semper, tellus sapien iaculis nunc, at
+    <input placeholder="..." class="answer" /> ipsum ante dictum lectus. Nunc eu
+    elementum ex, ut pretium leo. Praesent nibh leo, varius et facilisis sit
+    amet, pellentesque ac eros. Etiam odio lacus, dignissim ac nibh sit amet,
+    pellentesque rutrum quam. Duis placerat diam a turpis lacinia, id auctor
+    ipsum sollicitudin. Nulla consequat orci nec nulla facilisis, sed
+    condimentum diam imperdiet. Sed aliquam magna erat, et tempus ipsum egestas
+    ut.
+    <input placeholder="..." class="answer" /> elementum sodales malesuada. Quisque
+    vitae nunc eleifend, dignissim purus vitae, sollicitudin ex. Praesent fringilla
+    ultricies eros sit amet laoreet. Maecenas et lectus ex.
+  </p>
+
+  <p>
+    <span class="right">Bonne réponse</span> et
+    <span class="wrong">Mauvaise réponse</span>
+  </p>
+
+  <button class="button" on:click={verifier}>vérifier</button>
+</div>
+
+<style>
+  .answer {
+    font-weight: 900;
+    padding: 0 5px;
+  }
+
+  .wrong {
+    color: red;
+  }
+  .right {
+    color: green;
+  }
+</style>
