e2f766f434c85ca92353772aabc8fb110377e1cc
Laura <106605150+LauraR40@users.noreply.github.com>
2024-04-26 04:29:46 +0200

    du design

--- src/routes/fishdex/+page.svelte
+++ src/routes/fishdex/+page.svelte
@@ -6,9 +6,12 @@
   let poissons = $fishStore;
 </script>
 
-<div class="disposition">
+<div class="disposition-fishdex">
   <Profile />
   <div class="collection">
+    <h2 style="color: #476AAD; margin-bottom: 15px;">
+      Votre collection de poissons
+    </h2>
     {#each poissons as poisson}
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
@@ -29,14 +32,25 @@
   /* TODO faire le design */
 
   .collection {
-    border: 1px solid whitesmoke;
+    border: 1px solid;
     padding: 12px;
-    width: 90vw;
     background-color: #e8e7ff;
-    width: 90vw;
     border-radius: 20px;
     border: solid;
     border-color: #f7f6ff;
     border-width: 5px;
+    margin-bottom: 1000px;
+  }
+
+  .disposition-fishdex {
+    margin: 0;
+    justify-content: center;
+    display: grid;
+    grid-gap: 30px;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    overflow: scroll;
+    padding: 20px;
+    background-color: #c0d6ff;
+    height: 95vh;
   }
 </style>
