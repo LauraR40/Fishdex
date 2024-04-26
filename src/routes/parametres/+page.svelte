71a5096a8f62abcc49a30bb04537805081fb333b
Laura <106605150+LauraR40@users.noreply.github.com>
2024-04-26 03:22:54 +0200

    début design page param

--- src/routes/parametres/+page.svelte
+++ src/routes/parametres/+page.svelte
@@ -3,13 +3,51 @@
 </script>
 
 <div class="disposition">
-  <div class="der-poisson bloc-default gradiant-left"></div>
-  <div class="der-poisson bloc-default gradiant-left"></div>
-  <div class="der-poisson bloc-default gradiant-left"></div>
-  <div class="der-poisson bloc-default gradiant-left"></div>
-  <div class="der-poisson bloc-default gradiant-left"></div>
-  <div class="der-poisson bloc-default gradiant-left"></div>
-  <div class="der-poisson bloc-default gradiant-left"></div>
+  <div class="der-poisson bloc-default gradiant-left ligne space">
+    <div>
+      <img alt="avatarTemp" />
+    </div>
+    <div>
+      <p>Changer l’avatar</p>
+      <p>Vos preferences</p>
+    </div>
+  </div>
+  <div class="der-poisson bloc-default gradiant-left ligne space">
+    <div>
+      <img alt="niveau" />
+    </div>
+    <div>
+      <p>Vos récompenses</p>
+      <p>Des fonds d’écran</p>
+    </div>
+  </div>
+  <div class="der-poisson bloc-default gradiant-left">
+    <img alt="icone vibre" />
+    <p>Vibreur</p>
+    <p>Vibration quand le scan est validé.</p>
+    <img alt="bouton" />
+  </div>
+  <div class="der-poisson bloc-default gradiant-left">
+    <img alt="icone cloche" />
+    <p>Sonnerie</p>
+    <p>Sonne quand le scan est valide.</p>
+    <img alt="bouton" />
+  </div>
+  <div class="der-poisson bloc-default gradiant-left">
+    <img alt="valide" />
+    <p>Notez-nous</p>
+    <p>Votre meilleur récompense pour nous</p>
+  </div>
+  <div class="der-poisson bloc-default gradiant-left">
+    <img alt="partage" />
+    <p>Partager</p>
+    <p>Partager l’application avec d’autres.</p>
+  </div>
+  <div class="der-poisson bloc-default gradiant-left">
+    <img alt="info" />
+    <p>Politique de confidentialité</p>
+    <p>Suivez nos politiques qui vous sont favorables</p>
+  </div>
   <div class="fin-page"></div>
 </div>
 <Navbar />
