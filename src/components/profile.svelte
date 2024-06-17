c0e1f4568a340385d6e07581e4e4a1ec19234a01
Laura <106605150+LauraR40@users.noreply.github.com>
2024-06-17 15:05:39 +0200

    maj profil

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -12,10 +12,26 @@
     />
     <!--{$profileStore.avatarUrl} -->
     <h4 style="margin-left: 10px;">{$profileStore.nom}</h4>
-    <div style="margin-left: 20px;">{Math.trunc($profileStore.points / 2)}</div>
+    <div>
+      <div class="poly">
+        <span style="color: black; margin-left: 26%; font-weight: 700;"
+          >{Math.trunc($profileStore.points / 2)}</span
+        >
+      </div>
+    </div>
   </div>
   <div class="ligne">
     <img class="rond" alt="Point de couleur" src={rondvert} />
     <p>Voir les récompenses de niveau →</p>
   </div>
 </div>
+
+<style>
+  .poly {
+    background-image: url("src/lib/assets/Polygon 1.svg");
+    height: 20px;
+    width: 23px;
+    margin-left: 20px;
+    background-repeat: no-repeat;
+  }
+</style>
