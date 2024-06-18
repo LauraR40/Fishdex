597d5fb5562e7b25a434938a7af4b08f1458622c
Laura <106605150+LauraR40@users.noreply.github.com>
2024-06-18 11:15:42 +0200

    Ajout mentions légales

--- src/routes/politique/+page.svelte
+++ src/routes/politique/+page.svelte
@@ -2,6 +2,49 @@
   import Navbar from "../../components/navbar.svelte";
 </script>
 
-<h1>Mentions Légales</h1>
+<div
+  style="margin-left: 7vw;
+    margin-right: 7vw;
+    margin-bottom: 10vh;
+    height: 115vh;"
+>
+  <h1>Mentions Légales</h1>
+
+  <h2>Éditeur du site :</h2>
+
+  <p>
+    Propriétaire du site :<br />
+    Email :
+  </p>
+
+  <h2>Hébergement</h2>
+
+  <p>*Ajouter adresse hébergeur ici</p>
+
+  <h2>Propriété intellectuelle</h2>
+
+  <p>
+    Le contenu de ce site est protégé par les lois en vigueur sur la propriété
+    intellectuelle et le droit d'auteur. Toute reproduction non autorisée des
+    contenus, marques ou logos est interdite.
+  </p>
+
+  <h2>Données personnelles</h2>
+
+  <p>
+    Nous vous recommendons d'autoriser l'accès de votre caméra pour le bon
+    fonctionnement de l'application. <br /><br />
+    Nous ne collectons pas vos données, toute information vous concernant que vous
+    fournirez à l'application seront effacées une fois votre session terminée.
+  </p>
+
+  <h2>Liens hypertextes</h2>
+
+  <p>Ce site n'utilise pas de liens externes.</p>
+
+  <h2>Cookies</h2>
+
+  <p>Ce site n'utilise pas de cookies.</p>
+</div>
 
 <Navbar />
