896be74f78eaa80427a19188f6d16f3ded576394
Iziram <github@iziram.fr>
2024-06-22 15:38:26 +0200

    Nouvel affichage fishdex

--- src/routes/fishdex/+page.svelte
+++ src/routes/fishdex/+page.svelte
@@ -1,31 +1,45 @@
 <script>
   import Navbar from "$components/navbar.svelte";
   import Profile from "$components/profile.svelte";
-  import { checkConnected, profileStore } from "$lib/database";
+  import Poisson from "$components/poisson.svelte";
+  import { checkConnected, profileStore, getAllFish } from "$lib/database";
+
+  async function loadData() {
+    // On vérifie que l'utilisateur est connecté
+    await checkConnected();
+
+    // Récupération des poissons de la BDD
+    let tousLesPoissons = await getAllFish();
+
+    // On affiche d'abord les poissons déjà rencontrés
+    // Sinon on affiche les poissons non rencontrés en noir
+    tousLesPoissons = tousLesPoissons.sort((a, b) => {
+      const vu = $profileStore.poissons.find((p) => p.id === a.id);
+      const vu2 = $profileStore.poissons.find((p) => p.id === b.id);
+
+      a.vu = vu ? true : false;
+      b.vu = vu2 ? true : false;
+
+      return a.vu === b.vu ? 0 : a.vu ? -1 : 1;
+    });
+
+    return tousLesPoissons;
+  }
 </script>
 
 <!-- Page collection  -->
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-{#await checkConnected() then}
+{#await loadData() then poissons}
   <div class="disposition-fishdex">
     <!-- Profile de l'utilisateur  -->
     <Profile user={$profileStore} />
     <!-- Affichage des poissons rencontrés  -->
-    <div class="collection">
-      <h2 style="color: #476AAD; margin-bottom: 15px;">
-        Votre collection de poissons
-      </h2>
-      {#each $profileStore.poissons as poisson}
-        <img
-          style="width: 20vw;"
-          src={poisson.url}
-          alt={poisson.nom}
-          on:click={() => {
-            location.href = `/fishdex/${poisson.id}/desc`;
-          }}
-        />
+    <h2 style="color: #476AAD">Votre collection de poissons</h2>
+    <div id="collection">
+      {#each poissons as poisson}
+        <Poisson {poisson} />
       {/each}
     </div>
   </div>
@@ -36,26 +50,30 @@
 
 <!-- CSS de la page collection  -->
 <style>
-  .collection {
-    border: 1px solid;
-    padding: 12px;
-    background-color: #e8e7ff;
-    border-radius: 20px;
-    border: solid;
-    border-color: #f7f6ff;
-    border-width: 5px;
-    margin-bottom: 1000px;
+  h2 {
+    text-align: center;
+  }
+
+  #collection {
+    display: grid;
+    grid-template-columns: 45vw 45vw;
+    grid-template-rows: repeat(auto-fill, 45vw);
+    grid-gap: 2vw;
+
+    justify-items: center;
+    align-items: center;
   }
 
   .disposition-fishdex {
-    margin: 0;
-    justify-content: center;
-    display: grid;
-    grid-gap: 30px;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-    overflow: scroll;
-    padding: 20px;
-    background-color: #c0d6ff;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 2vh;
+
     height: 95vh;
+    padding: 20px;
+    overflow: scroll;
+    background-color: #c0d6ff;
+    padding-bottom: 10vh;
   }
 </style>
