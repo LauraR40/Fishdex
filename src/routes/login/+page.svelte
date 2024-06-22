f55b384a6a8878de6d6cf82402c3cffb0e444c79
Laura <106605150+LauraR40@users.noreply.github.com>
2024-06-22 17:03:47 +0200

    début design login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,16 +10,39 @@
 </script>
 
 <!-- Page de connexion -->
-<section>
-  <h1>Login</h1>
-  <form on:submit|preventDefault={handleForm}>
-    <input type="email" name="email" placeholder="Email" required />
-    <input type="password" name="password" placeholder="Password" required />
-    <button type="submit">Login</button>
-  </form>
-
-  <a href="/signup">Signup</a>
-</section>
+<div
+  style="background-image: url('/src/lib/assets/Vagues.svg'); background-repeat: no-repeat; background-size: 28rem; background-position: center; width: 100vw; gap:0;"
+  class="disposition"
+>
+  <h1
+    style=" width:80vw; color: rgba(38, 88, 179, 0.60);; font-size:40px; height: 3vh; font-weight:900; font-family: Outfit, sans-serif;"
+  >
+    La Quête de l'Aquarium
+  </h1>
+  <div class="glass-cadre">
+    <section>
+      <h2 style="color:white; text-align: center;">Connexion</h2>
+      <br />
+      <form on:submit|preventDefault={handleForm}>
+        <input type="email" name="email" placeholder="Email" required />
+        <input
+          style="margin-bottom: 10px;"
+          type="password"
+          name="password"
+          placeholder="Mot de passe"
+          required
+        />
+        <button class="submit" type="submit">Me connecter</button>
+      </form>
+      <br />
+      <a
+        style="color: white; text-decoration:none; font-weight:600;"
+        href="/signup">Je n'ai pas de compte ➔</a
+      >
+    </section>
+  </div>
+  <div></div>
+</div>
 
 <!-- CSS de la page de connexion -->
 <style>
@@ -43,5 +66,31 @@
   }
   button:hover {
     background-color: #0056b3;
+  }
+
+  .submit {
+    border-radius: 5px;
+    background-color: rgba(168, 192, 236, 0);
+    border-color: white;
+  }
+
+  .glass-cadre {
+    /* From https://css.glass */
+    background: rgba(38, 88, 179, 0.46);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(9.3px);
+    -webkit-backdrop-filter: blur(9.3px);
+    border: 1px solid rgba(38, 88, 179, 0.4);
+
+    width: 80vw;
+    height: 50vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  input {
+    background-color: white;
+    border-radius: 5px;
   }
 </style>
